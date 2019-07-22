bf1e903b54e3e46d510a3a5491402c70a9ddc935
Timur Surov <timursurov@gmail.com>
2019-07-22 22:53:22 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,5 +1,29 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.Exception
+
+private class EscData(
+    val symbol:Char,
+    val code:Int,
+    val mnemocode:String,
+    val hexCode:String = Integer.toHexString(code)){
+
+    companion object Factory{
+        private val dictEsc = listOf(
+            EscData('"',34, "quot"),
+            EscData('&' ,39, "amp"),
+            EscData('<',60, "lt"),
+            EscData('>',62, "gt"),
+            EscData('\'',8216, "lsquo"),
+            EscData('\'',8217, "rsquo")
+        )
+        fun parse(str:String):EscData?
+        {
+            return dictEsc.firstOrNull { data -> (str == data.mnemocode || str == "#x${data.hexCode}" || str == "#x${data.hexCode.toUpperCase()}" || str == "#${data.code}") }
+        }
+    }
+}
+
 fun String.truncate(length: Int = 16) : String {
     val ending = "..."
 
