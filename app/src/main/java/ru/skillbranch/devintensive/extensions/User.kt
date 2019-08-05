1d97b7fa3450ed9277a0a019b009cb954fbe468b
Timur Surov <timursurov@gmail.com>
2019-08-05 13:50:56 +0300

    hometask_4 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -3,26 +3,21 @@ package ru.skillbranch.devintensive.extensions
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
 
 fun User.toUserView(): UserView {
-    val fullName = "$firstName $lastName"
 
-    val nickName = Utils.transliteration(fullName)
+    val nickName = Utils.transliteration("$firstName $lastName")
     val initials = Utils.toInitials(firstName, lastName)
-    val status = if (lastVisit == null) {
-        "Ещё ни разу не был"
-    } else if (isOnline) {
-        "online"
-    } else {
-        "Последний раз был ${lastVisit.humanizeDiff()}"
-    }
-
+//    val status =
+//        if (lastVisit == null) "Еще ни разу не был" else if (isOnline) "online" else "В последний раз был ${lastVisit.humanizeDiff()}"
     return UserView(
         id,
-        fullName = fullName,
-        initials = initials,
-        nickName = nickName,
+        fullName = "$firstName $lastName",
         avatar = avatar,
-        status = status
+        nickName = nickName,
+        initials = initials
+//        status = status
     )
 }
+
